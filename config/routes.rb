13c4a16516aa6af2b7b42cf03678837573ab96ba
Christopher <chrisabbod@gmail.com>
2022-04-03 21:01:04 -0400

    Add show article functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'family', to: 'pages#family'
+  #get 'articles', to: 'pages#articles'
+  #resources :articles
+  resources :articles, only: [:show]
 end
