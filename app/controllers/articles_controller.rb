bc1511a3a9cf2e79f652711ca1c6c0cedcb6f014
Christopher <chrisabbod@gmail.com>
2022-04-08 23:58:31 -0400

    Added ability to delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,39 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all 
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create 
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully." #can choose any name to give flash message but common symbols are :notice or :alert. Alert is usually for when something goes wrong.
+      redirect_to @article #redirect using the show method (@article gives article id)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updates successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'family', to: 'pages#family'
-  #get 'articles', to: 'pages#articles'
-  #resources :articles
-  resources :articles, only: [:show, :index]
+  resources :articles
 end
